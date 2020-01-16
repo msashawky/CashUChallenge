8b14a52bb532c0a3e9675625d96403e61fab306a
”msashawky” <msashawky@gmail.com>
2020-01-16 19:37:22 +0200

    Adding UnitTest with phpUnit

---
+++ tests/BestHotelsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class BestHotelsTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->call('POST', '/api/hotel/topHotels');
+        $this->assertEquals(200, $response->status());
+        $this->assertTrue(true);
+    }
+}
